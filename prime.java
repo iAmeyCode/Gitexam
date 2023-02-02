13068554525ed214efee3c2ce84b9b1ce36d4c9e
iAmeyCode <ameybhoite007@gmail.com>
2023-02-02 15:58:46 +0530

    commit to javaOne

--- prime.java
+++ prime.java
@@ -1,24 +1,3 @@
-import javafx.stage.StageStyle;
-
-class Fibonacci{
-
-    public static void main(String args[]){
-
-        int n1 =0;
-        int n2 =1;
-        int n3;
-        int count=20;
-
-        for(int i = 2; i<count ; i++ ){
-            n3= n1+n2;
-            System.out.println(" "+n3);
-            n1=n2;
-            n2=n3;
-        }
-
-
-    }
-}
 
 
 class Prime{
@@ -44,7 +23,9 @@ class Prime{
                 System.out.println(i +" ");
             }
         }
-        
+
     }
 
 }
+        
+    
